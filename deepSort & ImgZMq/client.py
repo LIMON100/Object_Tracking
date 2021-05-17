aefc029fba4a759ea2a64da04e95e94a544f268a
limon100 <mahmudurlimon41@gmail.com>
2021-05-17 11:53:38 +0600

    tracking with imgzmq

---
+++ deepSort & ImgZMq/client.py
@@ -0,0 +1,26 @@
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import socket
+import time
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", required=True,
+	help="ip address of the server to which the client will connect")
+args = vars(ap.parse_args())
+
+
+sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+	args["server_ip"]))
+
+
+rpiName = socket.gethostname()
+vs = VideoStream(src = 0).start()
+#vs = VideoStream("data/video/input_video.mp4")
+time.sleep(2.0)
+
+
+while True:
+	frame = vs.read()
+	sender.send_image(rpiName, frame)
